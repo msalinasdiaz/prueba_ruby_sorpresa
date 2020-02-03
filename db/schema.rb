57e9384a51b68992d30361c07b0af29abbc79d70
rkgpi <msalinasdiaz@gmail.com>
2020-02-03 11:45:21 -0300

    commit parte 2 prueba CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2020_02_03_133953) do
 
   create_table "todos", force: :cascade do |t|
     t.string "description"
-    t.boolean "complete"
+    t.boolean "completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
