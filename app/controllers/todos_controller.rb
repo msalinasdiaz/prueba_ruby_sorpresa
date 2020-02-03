6f14712d602d68611f3a489fb835aa4e14a7d051
rkgpi <msalinasdiaz@gmail.com>
2020-02-03 13:32:37 -0300

    edit option

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -16,11 +16,22 @@ class TodosController < ApplicationController
     end
     def show
         @todo = Todo.find(params[:id])
-
-    end    
+    end 
+    def edit
+        @todo = Todo.find(params[:id])
+    end
+    def update
+        @todo = Todo.find(params[:id])
+        if @todo.update (todo_params)
+            flash[:notice] = "Your new tarea was updated succesful"
+            redirect_to root_path(@todo)
+        else
+            render 'edit'
+        end
+    end   
 private
     def todo_params
         params.require(:todo).permit(:description, :completed)
-    
+        
     end
 end
